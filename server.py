9776ba5bc49eefa75031c9326ee823185985bcba
Ines <inesmersak@gmail.com>
2016-05-14 22:49:56 +0200

    Added basic website.

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='./static')
+
+@route('/')
+def main():
+    return template('main')
+
+@route('/statistics/')
+def statistics():
+    games_played = 0
+    optimal_games_played = 0
+    avg_opt_sol_len = 0
+    return template('statistics', games_played=games_played,
+                    optimal_games_played=optimal_games_played,
+                    avg_opt_sol_len=avg_opt_sol_len)
+
+@route('/about/')
+def about():
+    return template('about')
 
 run(host='localhost', port=8080)
---
+++ views/main.tpl
@@ -0,0 +1,13 @@
+%rebase base
+<div class="jumbotron">
+  <h1>Helping determine disease progress</h1>
+  <p>The Tower of London puzzle was invented in 1982 as a psychological test. It has been used as one of the
+    tests to determine progress of Parkinson's and Alzheimer's disease ever since.</p>
+  <p>
+    We present an easy-to-use version of the test that tracks your results and analyses your progress.
+  </p>
+  <p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Run test</a>
+    <a class="btn btn-primary btn-lg" href="statistics/" role="button">View statistics</a>
+  </p>
+</div>
